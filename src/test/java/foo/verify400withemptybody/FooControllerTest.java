eddb29fa4edee168ff006ed5f8ac6493701ce697
Marc Paquette <Marc.Paquette@trilliantinc.com>
2018-08-22 15:46:08 -0400

    remove leftover import

--- src/test/java/foo/verify400withemptybody/FooControllerTest.java
+++ src/test/java/foo/verify400withemptybody/FooControllerTest.java
@@ -1,7 +1,6 @@
 package foo.verify400withemptybody;
 
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
