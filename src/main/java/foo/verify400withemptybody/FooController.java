78dc73e7b60519d7710a2a8e802139c1fa12687f
Marc Paquette <Marc.Paquette@trilliantinc.com>
2018-08-22 17:31:35 -0400

    remove @ExceptionHandler

--- src/main/java/foo/verify400withemptybody/FooController.java
+++ src/main/java/foo/verify400withemptybody/FooController.java
@@ -1,16 +1,12 @@
 package foo.verify400withemptybody;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.server.ServerWebInputException;
 
 import java.net.URI;
 
@@ -19,16 +15,6 @@ import java.net.URI;
 public class FooController {
 
   private static String foos;
-
-  @ExceptionHandler(value = { ServerWebInputException.class,
-                              NullPointerException.class,
-                              IllegalStateException.class,
-                              IllegalArgumentException.class })
-  public @ResponseBody
-  ResponseEntity sendBadRequest(Exception exception) {
-    System.err.println(exception.getMessage());
-    return new ResponseEntity(HttpStatus.BAD_REQUEST);
-  }
 
   @PostMapping
   public ResponseEntity<String> createFoo(@RequestBody String dto) {
